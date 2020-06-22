ecd2b88e1430489ab570d29782696b70267afc78
Daniela Ramirez <daniedi.solution@gmail.com>
2020-06-22 13:47:05 -0500

    modificacion ventada modal

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
 import validator from "./validator.js";
 
 let tarjeta = document.getElementById("tarjeta"),
-formulario = document.getElementById("formulario"),
-logoMarca = document.getElementById("logoMarca")
+  formulario = document.getElementById("formulario"),
+  logoMarca = document.getElementById("logoMarca")
 
 
-  tarjeta.addEventListener("click", () =>{
-    if(tarjeta.classList.contains('active')){
-      tarjeta.classList.remove('active');
-    }else {
-      tarjeta.classList.add('active');
-    }
-  })
-   
- 
+tarjeta.addEventListener("click", () => {
+  if (tarjeta.classList.contains('active')) {
+    tarjeta.classList.remove('active');
+  } else {
+    tarjeta.classList.add('active');
+  }
+})
+
+
 
 
 //**select del mes generado dinamicamente */
-for(let i = 1; i <= 12; i++){
-let opcion = document.createElement("option");
-  opcion.value = i; /**aqui va cambiando por cada interraccion hasta llegar a 12, asi generamos los 12 elementos aqui en js y no directamente en html*/
-  opcion.innerText = i; /**para trabajar con el texto que va ir dentro de nuestra opcion */
-  formulario.selectMes.appendChild(opcion);  /**aqui accedo al id selectMes, formulario ya es uan variable definidad  y el appenchil nos permite ponerle dentro  del select mes nuestra opcion*/
+
+for (let i = 1; i <= 12; i++) {
+  let opcion = document.createElement("option");
+  opcion.value = i;
+  opcion.innerText = i;
+  formulario.selectMes.appendChild(opcion);
 
 }
 //**select del año dinamicamente */
 
 const yearActual = new Date().getFullYear();/**aqui esta guardado el año actual en la variable el sistema lo arroja cumpleto */
-for(let i = yearActual; i <= yearActual + 8; i ++){/* i va a comezar en el año actual y le sumamos 8 para que nuestro siclo se repita o sea suma 8 años mas */
-let opcion = document.createElement("option");
-opcion.value = i;
-opcion.innerText = i; 
-formulario.selectYear.appendChild(opcion);
+for (let i = yearActual; i <= yearActual + 8; i++) {
+  let opcion = document.createElement("option");
+  opcion.value = i;
+  opcion.innerText = i;
+  formulario.selectYear.appendChild(opcion);
 }
 
-function loadModal(){
+function loadModal() {
+  var modal = document.getElementById("myModal");
   modal.style.display = "block";
 }
 
-document.getElementById("close").addEventListener("click", () =>{
+myModal.addEventListener("click", () => {
+  var modal = document.getElementById("myModal");
   modal.style.display = "none";
 })
 
@@ -54,23 +57,22 @@ function tomarNumerodeTarjeta() {
     loadModal()
   } else {
     let valido = validator.isValid(numeroDeTarjeta);
-    if(valido){
+    if (valido) {
       document.getElementById("numero").innerHTML = `${validator.maskify(
         numeroDeTarjeta
       )}`;
       document.getElementById("nombre").innerHTML = nombreDeTarjeta;
-      
       const imagen = document.createElement("img");
-      if(numeroDeTarjeta.charAt(0) == 4){
-          imagen.src = 'imagenes/visa.png';
-        } else if(numeroDeTarjeta.charAt(0) == 5){
-          imagen.src = "imagenes/mastercard.png";
-        } else {
-          imagen.src = "imagenes/banco.png";
-        }
-        logoMarca.appendChild(imagen);
+      if (numeroDeTarjeta.charAt(0) == 4) {
+        imagen.src = 'imagenes/visa.png';
+      } else if (numeroDeTarjeta.charAt(0) == 5) {
+        imagen.src = "imagenes/mastercard.png";
       } else {
-        loadModal()
+        imagen.src = "imagenes/banco.png";
+      }
+      logoMarca.appendChild(imagen);
+    } else {
+      loadModal()
     }
   }
 }
@@ -79,9 +81,9 @@ console.log(validator);
 
 
 
-  
 
- 
+
+
 
 
 
